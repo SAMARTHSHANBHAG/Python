e9ba03d80299cb3ae6de23151158c3264cbd9985
SAMARTHSHANBHAG <samarthr.shanbhag@gmail.com>
2025-03-26 22:54:11 +0530

    -

--- Conditionals/conditional_basics.py
+++ Conditionals/conditional_basics.py
@@ -1,8 +1,8 @@
-age = int(input("Enter your age: "))
-if(age >= 18):
-    print("Adult")
-else:
-    print("Not an adult")
+# age = int(input("Enter your age: "))
+# if(age >= 18):
+#     print("Adult")
+# else:
+#     print("Not an adult")
 
 
 
